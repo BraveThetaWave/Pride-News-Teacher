1a4faf8017daf7dd2058484e10faefea78da446c
BraveThetaWave <sam@my.innovationspokane.org>
2018-04-26 11:21:45 -0700

    Connected Buttons

---
+++ Pride-News-Teacher/Pride-News-Teacher/SendNotifications.swift
@@ -0,0 +1,37 @@
+//
+//  SendNotifications.swift
+//  Pride-News-Teacher
+//
+//  Created by CS Student on 4/26/18.
+//  Copyright © 2018 PrideSchools. All rights reserved.
+//
+
+import UIKit
+
+class SendNotifications: UIViewController {
+    @IBAction func Send(_ sender: UIButton, forEvent event: UIEvent) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pride-News-Teacher/Pride-News-Teacher/ViewController.swift
+++ Pride-News-Teacher/Pride-News-Teacher/ViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var num = 0
+    var clicks = 0
+    var mod : Int = 1
     @IBAction func ClickIt(_ sender: UIButton, forEvent event: UIEvent) {
-        Thing.text = ("HI")
+        clicks+=1
+        let numAsString: String = String(describing:num)
+        Thing.text = numAsString
+        
+    }
+    @IBAction func x2(_ sender: UIButton, forEvent event: UIEvent) {
+        mod *= 2
     }
     @IBOutlet weak var Thing: UILabel!
     
